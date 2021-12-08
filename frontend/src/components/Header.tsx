45dd08028d418c5e55ce43490d1650e67961f438
Hidayat <hidayetmmdff@gmail.com>
2021-12-08 22:49:28 +0400

    dark theme fix

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,18 +1,21 @@
-import { Header, Button, useMantineTheme } from "@mantine/core";
+import { Header, Title, useMantineTheme, Switch } from "@mantine/core";
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
+import classes from "./Header.module.scss";
 
 const HeaderComponent = () => {
   const { colorScheme } = useMantineTheme();
   const { toggleDark } = useContext(ThemeContext);
 
   return (
-    <Header height={60} padding="xs">
-      <Button
-        onClick={() => toggleDark(colorScheme === "light" ? true : false)}
-      >
-        Light/Dark
-      </Button>
+    <Header fixed className={classes.header} height={60} padding="lg">
+      <Title order={3}>Places</Title>
+      <Switch
+        checked={colorScheme === "dark" ? true : false}
+        onChange={() => toggleDark(colorScheme === "light" ? true : false)}
+        label={<span className="material-icons-outlined">dark_mode</span>}
+        className={classes.switch}
+      />
     </Header>
   );
 };
