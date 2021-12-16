73cf69a4eada9cdd22cc6bf28104698cf51d8a1b
Hidayat <hidayetmmdff@gmail.com>
2021-12-16 14:58:46 +0400

    PlacesHome animation for places

--- frontend/src/containers/PlacesHome/PlacesHome.tsx
+++ frontend/src/containers/PlacesHome/PlacesHome.tsx
@@ -9,33 +9,51 @@ import {
   Grid,
   Col,
   useMantineTheme,
-  Skeleton,
 } from "@mantine/core";
 import classes from "./PlacesHome.module.scss";
 import axios, { AxiosResponse, AxiosError } from "axios";
-import { useEffect, useContext, useState } from "react";
+import { useEffect, useContext, useRef } from "react";
 import { useNotifications } from "@mantine/notifications";
 import { AuthContext, Place } from "../../context/AuthContext";
+import { spring } from "react-flip-toolkit";
 
 const PlacesHome = () => {
-  const [loading, setLoading] = useState(false);
   const { places, setPlaces, fetchPlacesToggle } = useContext(AuthContext);
   const theme = useMantineTheme();
   const notifications = useNotifications();
   const secondaryColor =
     theme.colorScheme === "dark" ? theme.colors.dark[1] : theme.colors.gray[7];
 
+  const containerRef: any = useRef(null);
   useEffect(() => {
-    fetchPlaces();
+    fetchPlaces().then(() => {
+      const boxes = [...containerRef.current.querySelectorAll(".boxItem")];
+      boxes.forEach((el, i) => {
+        spring({
+          config: "gentle",
+          values: {
+            translateY: [-15, 0],
+            opacity: [0, 1],
+          },
+          onUpdate: (props: any) => {
+            el.style.opacity = props.opacity;
+            el.style.transform = `translateY(${props.translateY}px)`;
+          },
+          // delay: i * 25,
+          delay: i * 100,
+          onComplete: () => {
+            // add callback logic here if necessary
+          },
+        });
+      });
+    });
   }, [fetchPlacesToggle]);
 
   const fetchPlaces = async () => {
-    setLoading(true);
     try {
       const fetchedPlaces: AxiosResponse = await axios.get("/places");
-      setLoading(false);
 
-      if (!places) {
+      if (!fetchedPlaces) {
         return notifications.showNotification({
           message: "Couldn't find any place",
           color: "red",
@@ -44,8 +62,6 @@ const PlacesHome = () => {
 
       setPlaces(fetchedPlaces.data?.places.reverse());
     } catch (err: AxiosError | any) {
-      setLoading(false);
-
       if (axios.isAxiosError(err)) {
         console.log(err?.response);
         notifications.showNotification({
@@ -63,59 +79,61 @@ const PlacesHome = () => {
 
   return (
     <Container className={classes.container} size="xl">
-      <Grid>
+      <Grid ref={containerRef}>
         {places.map((place: Place) => (
-          <Col key={place.id} span={3}>
-            <Skeleton visible={loading}>
-              <Card shadow="sm" padding="lg">
-                <Card.Section>
-                  <Image
-                    src={place.image}
-                    height={180}
-                    alt={place.title}
-                    fit="contain"
-                  />
-                </Card.Section>
-                <Group
-                  position="apart"
-                  style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
-                >
-                  <Text weight={500}>{place.title}</Text>
-                  <Badge color="pink" variant="light">
-                    On Sale
-                  </Badge>
-                </Group>
-                <Text
-                  lineClamp={3}
-                  size="sm"
-                  style={{
-                    color: secondaryColor,
-                    lineHeight: 1.5,
-                    minHeight: 65,
-                    maxHeight: 65,
-                  }}
-                >
-                  {place.description}
-                </Text>
-                <Text
-                  lineClamp={1}
-                  size="xs"
-                  style={{
-                    color: secondaryColor,
-                  }}
-                >
-                  {place.address}
-                </Text>
-                <Button
-                  variant="light"
-                  color="blue"
-                  fullWidth
-                  style={{ marginTop: 14 }}
-                >
-                  Book classic tour now
-                </Button>
-              </Card>
-            </Skeleton>
+          <Col
+            key={place.id}
+            span={3}
+            className={`boxItem ${classes.opacity_0}`}
+          >
+            <Card shadow="sm" padding="lg">
+              <Card.Section>
+                <Image
+                  src={place.image}
+                  height={180}
+                  alt={place.title}
+                  fit="contain"
+                />
+              </Card.Section>
+              <Group
+                position="apart"
+                style={{ marginBottom: 5, marginTop: theme.spacing.sm }}
+              >
+                <Text weight={500}>{place.title}</Text>
+                <Badge color="pink" variant="light">
+                  On Sale
+                </Badge>
+              </Group>
+              <Text
+                lineClamp={3}
+                size="sm"
+                style={{
+                  color: secondaryColor,
+                  lineHeight: 1.5,
+                  minHeight: 65,
+                  maxHeight: 65,
+                }}
+              >
+                {place.description}
+              </Text>
+              <Text
+                lineClamp={1}
+                size="xs"
+                style={{
+                  color: secondaryColor,
+                }}
+              >
+                {place.address}
+              </Text>
+              <Button
+                variant="light"
+                color="blue"
+                fullWidth
+                style={{ marginTop: 14 }}
+              >
+                Book classic tour now
+              </Button>
+            </Card>
           </Col>
         ))}
       </Grid>
